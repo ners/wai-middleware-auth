cb9de169e96906293cec3a12ff6fc03d5d2c94f8
German Zhyvotnikov <german.zhivotnikov@gmail.com>
2025-06-23 18:27:30 +0300

    update uri of google logo

--- src/Network/Wai/Middleware/Auth/OAuth2/Google.hs
+++ src/Network/Wai/Middleware/Auth/OAuth2/Google.hs
@@ -51,8 +51,7 @@ mkGoogleProvider clientId clientSecret emailWhiteList mProviderInfo =
     defProviderInfo =
       ProviderInfo
       { providerTitle = "Google"
-      , providerLogoUrl =
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/200px-Google_%22G%22_Logo.svg.png"
+      , providerLogoUrl = "https://www.google.com/favicon.ico"
       , providerDescr = "Use your Google account to access this page."
       }
 
